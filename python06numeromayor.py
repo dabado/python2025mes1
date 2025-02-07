8b15616fa1bc791a874808595a023c110511b5cc
dabado <davidbarroso@telefonica.net>
2025-02-07 20:23:06 +0100

    mayor

---
+++ python06numeromayor.py
@@ -0,0 +1,13 @@
+print(f"introduce un numero1")
+num1=int(input())
+print(f"introduce un numero2")
+num2=int(input())
+if (num1 > num2 ):
+    print(f"el numero1 - {num1} - es mayor que numero2 - {num2} -")
+elif (num1 < num2):
+    print(f"el numero1 - {num1} - es menor que numero2 - {num2} -")
+else:
+    print(f"el numero1 - {num1} - es igual que que numero2 - {num2} -")
+
+
+     
