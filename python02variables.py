ab45828c604970169f85f3a5eae27ae1259a4af5
dabado <davidbarroso@telefonica.net>
2025-02-07 19:23:27 +0100

    mates

--- python02variables.py
+++ python02variables.py
@@ -3,16 +3,16 @@ import sys
 print(f"este es el primer mensaje de {sys.platform}")
 x = "python0"
 z = "Faltaria por crear : "
-ai = "(i)"
+
 for i in range(10):
     print(f"{z}  {x}{i}.py")
 
 print(type(x))
 print(type(z))
 print(type(i))
-#print(type(ai))
+ai = str({i})
 
-
+print(f"integrer a string", type(ai))
 
 
 
---
+++ python03mates.py
@@ -0,0 +1,14 @@
+print(":")
+numero1 = 22
+numero2 = 2
+suma = numero1 + numero2
+multiplicacion = numero1 * numero2
+division= numero1 / numero2
+resta = numero1 - numero2
+redondeadivision=int(division)
+print(f"resta", resta)
+print(f"multiplicacion", multiplicacion)
+print(f"suma", suma)
+print(f"division", division)
+print(f"redondeadivision {redondeadivision}")
+print(input("introduce un numero"))
