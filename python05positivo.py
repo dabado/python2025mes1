8af17b9aa48a7164194386253a834549ff898b81
dabado <davidbarroso@telefonica.net>
2025-02-07 20:13:50 +0100

    parentesis

--- python05positivo.py
+++ python05positivo.py
@@ -1,8 +1,8 @@
 print(f"introduce un numero")
 num=int(input())
-if num == 0:
+if (num == 0 ):
     print(f"el numero {num} es neutro")
-elif num < 0:
+elif (num < 0):
     print(f"el numero {num} es negativo")
 else:
     print(f"el numero {num} es positivo")
