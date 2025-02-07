84d0db515c4c65923b17e13e4b954abf84abae0c
DavidB <davidbarroso@telefonica.net>
2025-02-07 19:02:53 +0100

    Add files via upload

---
+++ python01test.py
@@ -0,0 +1,7 @@
+
+import sys 
+print(f"este es el primer mensaje de {sys.platform}")
+
+for i in range(10):
+    print(f"faltan por crear  python0{i}.py")
+
---
+++ python02variables.py
@@ -0,0 +1,20 @@
+
+import sys 
+print(f"este es el primer mensaje de {sys.platform}")
+x = "python0"
+z = "Faltaria por crear : "
+ai = "(i)"
+for i in range(10):
+    print(f"{z}  {x}{i}.py")
+
+print(type(x))
+print(type(z))
+print(type(i))
+#print(type(ai))
+
+
+
+
+
+
+
