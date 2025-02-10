62502b2bba982fa43f52978cdde1741bb8cbbfcf
dabado <davidbarroso@telefonica.net>
2025-02-10 20:17:54 +0100

    dbd

--- python011_horasextra.py
+++ python011_horasextra.py
@@ -4,11 +4,19 @@ print(f"introduce precio de la hora trabajada")
 precioHora = int(input())
 print(f"introduce los kilometros")
 kilometros = int(input())
+
+
 horasSemanales = 40
 salarioBase = horasSemanales * precioHora
 
-horasExtra = (horas  - horasSemanales ) * (precioHora + 2)
+
+if horas < 40 :
+    horasExtra = 0
+elif horas >= 40:
+    horasExtra = (horas  - horasSemanales ) * (precioHora + 2)   
+
 salarioTotal= salarioBase + horasExtra
+
 
 if kilometros <= 100:
     dietas = "locales"
@@ -28,13 +36,13 @@ elif salarioBase >= 600:
     retenciones = 40
 
 print("--------")
-print(f"dietas:{dietas}; retencion:{retenciones} %;")
+print(f"dietas:{dietas};                    retencion:  {retenciones} %")
 print("--------")
-print(f"salario Base : {salarioBase} euros, Horas Extra : {horasExtra} euros")
+print(f"salario Base :      {salarioBase} euros,     Horas Extra :   {horasExtra} euros")
 print("--------")
-print(f"el salario total es : {salarioTotal}")
-print("--------")
-print("--------")
+print(f"el salario total es :   {salarioTotal} euros ")
+print("                             --------")
+print("                             --------")
 
 
 
