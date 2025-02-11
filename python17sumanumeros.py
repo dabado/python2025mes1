b4dc8b58b7dab7c0c85e81584640dde1c420521e
dabado <davidbarroso@telefonica.net>
2025-02-11 19:15:16 +0100

    dbd

--- python17sumanumeros.py
+++ python17sumanumeros.py
@@ -2,8 +2,8 @@ print(f"introduce el texto numerico")
 numero = input()
 lista=[]
 sum = 0
-for i in numero[:]:
-    sum= sum + int(i)
+for i in numero:
+    sum = sum + int(i)
     lista.append(int(i))
 print(lista)
 print(f"suma: " ,sum)
