dbcd733e18b45f2d91baea5fa80a6bc1be3e6514
dabado <davidbarroso@telefonica.net>
2025-02-11 17:04:13 +0100

    bdbd

--- python15tablademultiplixar.py
+++ python15tablademultiplixar.py
@@ -1,8 +1,18 @@
-print(f"introduce el  numero inicial ")
+print(f"introduce el  numero inicial for")
 numero = int(input())
 
 for num in range(1, 11):
     resultado=numero * num
     print(f" {numero} por {num} = {resultado}")
 
+print(f"introduce el  numero inicial while")
+numero2 = int(input())
 
+contador = 1
+while (contador < 11):
+    result=numero2 * contador
+    print(f"{numero2} por {contador} = {result}")
+    contador += 1
+
+
+# ss = numero1 * contador for numero2
