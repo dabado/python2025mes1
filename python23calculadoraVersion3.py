38130183637be12f7bd45709d69b8e2fb0e93ed0
dabado <davidbarroso@telefonica.net>
2025-02-12 20:41:49 +0100

    DBDimportados

--- python23calculadoraVersion3.py
+++ python23calculadoraVersion3.py
@@ -17,19 +17,38 @@ def mostrarMenu():
     print("1_ sumar")
     print("2_ restar")
     print("3_ multiplica")
-    
+    print("4_ introducir numeros de nuevo")
+
+
     
 print("Metodos return")
+
 print("Introduzca el primer numnero")
 num1=input()
+while num1.isdigit() == False:
+    print("tiene que introducir un numero1")
+    num1=input()
+print(f"numero1 guardado {num1}")
+
+
+      
 print("introduzca el segundo numero")
 num2=input()
+while num2.isdigit() == False:
+    print("tiene que introducir un numero2")
+    num2=input()
+print(f"numero2 guardado {num2}")
+
+
+
+
 #mostrarMenu()
 opcion=99
 
 while (opcion != 0):
     mostrarMenu()
     opcion=int(input())
+
     if opcion == 1:
             resultado=sumar(num1, num2)
             print(resultado)
@@ -39,6 +58,11 @@ while (opcion != 0):
     elif opcion == 3:
             resultado = multiplicar(num1, num2)
             print(resultado)
+    elif opcion == 4:
+            print("Introduzca el primer numnero")
+            num1=input()
+            print("introduzca el segundo numero")
+            num2=input()
     elif opcion == 0:
             resultado = "ha elegido salir .. saliendo" 
             print(resultado)
---
+++ python24import.py
@@ -0,0 +1,9 @@
+import python23calculadoraVersion3metodos
+
+python23calculadoraVersion3metodos.mostrarMenu()
+
+
+            
+    
+#print(resultado)
+print("final de programa")
