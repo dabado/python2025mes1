5134e903d41ae35170341889159f7a28a78b5b86
dabado <davidbarroso@telefonica.net>
2025-02-25 16:14:42 +0100

    dbd

--- ORACLE/python43incrementoSalarioHospital.py
+++ ORACLE/python43incrementoSalarioHospital.py
@@ -1,45 +1,51 @@
 import oracledb
-
-conexion=(oracledb.connect( user="system", password="oracle",dsn="localhost/xe"))
-print(f".....conexion correcta { conexion}")
-
-
-print("indicanos el codigo de hospital a incrementar")
-hospitalCod=int(input())
-print(f"indicanos  la cantidad a incrementar al salario para hospital codigo : {hospitalCod}")
-incremento=int(input())
-sql="""
-    select * from PLANTILLA
-    where HOSPITAL_COD=:hospitalCod
-    """
-#print(f"sentencia: {sql}")
-cursor = conexion.cursor()
-cursor.execute(sql, (hospitalCod, ))
-for fila in cursor:
-    print(fila)
-print("encontrados :" + str(cursor.rowcount))
-print(cursor.bindnames())
-
-# no cierro el cursor para hacer recuperar resultado cursor.close()
-
-sqlupdate = """
-            update PLANTILLA 
-            set SALARIO = salario + :incremento 
-            where HOSPITAL_COD = :hospitalCod
-            """
-cursorupdate = conexion.cursor()
+def aumentoSalarioPorHospital():
+        
+    conexion=(oracledb.connect( user="system", password="oracle",dsn="localhost/xe"))
+    print(f".....conexion correcta { conexion}")
 
 
-cursorupdate.execute(sqlupdate, (incremento, hospitalCod))
+    print("indicanos el codigo de hospital a incrementar")
+    hospitalCod=int(input())
+    print(f"indicanos  la cantidad a incrementar al salario para hospital codigo : {hospitalCod}")
+    incremento=int(input())
+    sql="""
+        select APELLIDO, FUNCION , SALARIO , SALA_COD
+        from PLANTILLA
+        where HOSPITAL_COD=:hospitalCod
+        """
+    #print(f"sentencia: {sql}")
+    cursor = conexion.cursor()
+    cursor.execute(sql, (hospitalCod, ))
+    for fila in cursor:
+        print(fila)
+    print("encontrados :" + str(cursor.rowcount))
+    print(cursor.bindnames())
 
-print("modificados : incrementados "+ str(incremento) + " en " + str(cursorupdate.rowcount) + " modificados." )
+    # no cierro el cursor para hacer recuperar resultado cursor.close()
+
+    sqlupdate = """
+                update PLANTILLA 
+                set SALARIO = salario + :incremento 
+                where HOSPITAL_COD = :hospitalCod
+                """
+    cursorupdate = conexion.cursor()
+
+    
+    cursorupdate.execute(sqlupdate, (incremento, hospitalCod))
+
+    print("modificados : incrementados "+ str(incremento) + "€ en " + str(cursorupdate.rowcount) + " modificados." )
 
 
-print(cursorupdate.bindnames())
-cursor.execute(sql, (hospitalCod, ))
-for fila in cursor:
-    print(fila)
-conexion.commit()
-cursor.close()
-cursorupdate.close()
-conexion.close()
+    print(cursorupdate.bindnames())
+    # es necesario execute ??
+    
+    cursor.execute(sql, (hospitalCod, ))
+    for fila in cursor:
+        print(fila)
+    conexion.commit()
+    cursor.close()
+    cursorupdate.close()
+    conexion.close()
+
+aumentoSalarioPorHospital()
