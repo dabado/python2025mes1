1c3637a81cb59ff78a5d9ed98e374e79ad91aa50
DavidB <davidbarroso@telefonica.net>
2025-02-25 13:18:06 +0100

    Update python43incrementoSalarioHospital.py

--- ORACLE/python43incrementoSalarioHospital.py
+++ ORACLE/python43incrementoSalarioHospital.py
@@ -8,53 +8,33 @@ print("indicanos el codigo de hospital a incrementar")
 hospitalCod=int(input())
 print(f"indicanos  la cantidad a incrementar al salario para hospital codigo : {hospitalCod}")
 incremento=int(input())
-sql="select * from PLANTILLA where HOSPITAL_COD=:hospitalCod"
+sql="""
+    select * from PLANTILLA
+    where HOSPITAL_COD=:hospitalCod
+    """
 #print(f"sentencia: {sql}")
 cursor = conexion.cursor()
 cursor.execute(sql, (hospitalCod, ))
 for fila in cursor:
     print(fila)
 print("encontrados :" + str(cursor.rowcount))
+print(cursor.bindnames())
+
+# no cierro el cursor para hacer recuperar resultado cursor.close()
+
+sqlupdate = """
+            update PLANTILLA 
+            set SALARIO = salario + :incremento 
+            where HOSPITAL_COD = :hospitalCod
+            """
+cursorupdate = conexion.cursor()
 
 
-cursor.close()
+cursorupdate.execute(sqlupdate, (incremento, hospitalCod))
 
-sqlupdate = "update PLANTILLA set SALARIO = salario + :sala where HOSPITAL_COD = :hospitalCod" 
-cursor = conexion.cursor()
-cursor.execute(sqlupdate, (incremento, hospitalCod))
+print("modificados :" + str(cursorupdate.rowcount))
 
-
-print("modificados :" + str(cursor.rowcount))
-import oracledb
-
-conexion=(oracledb.connect( user="system", password="oracle",dsn="localhost/xe"))
-print(f".....conexion correcta { conexion}")
-
-
-print("indicanos el codigo de hospital a incrementar")
-hospitalCod=int(input())
-print(f"indicanos  la cantidad a incrementar al salario para hospital codigo : {hospitalCod}")
-incremento=int(input())
-
-
-sql="select * from PLANTILLA where HOSPITAL_COD=:hospitalCod"
-#print(f"sentencia: {sql}")
-cursor = conexion.cursor()
-cursor.execute(sql, (hospitalCod, ))
-for fila in cursor:
-    print(fila)
-print("encontrados :" + str(cursor.rowcount))
-cursor.close()
-
-
-sqlupdate = "update PLANTILLA set SALARIO = salario + :sala where HOSPITAL_COD = :hospitalCod" 
-cursor = conexion.cursor()
-cursor.execute(sqlupdate, (incremento, hospitalCod))
-print("modificados :" + str(cursor.rowcount))
 print(cursorupdate.bindnames())
-
-
-
 cursor.execute(sql, (hospitalCod, ))
 for fila in cursor:
     print(fila)
